e37eb283e16929cd75deee6f40ee929a4c6a161d
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-04-13 20:37:16 +0300

    update styles

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
+import { StyledButton } from './FeedbackOptions.styled';
 
 export const FeedbackOptions = ({ onLeaveFeedback }) => {
   return (
     <>
-      <button type="button" name="good" onClick={onLeaveFeedback}>
+      <StyledButton type="button" name="good" onClick={onLeaveFeedback}>
         Good
-      </button>
-      <button type="button" name="neutral" onClick={onLeaveFeedback}>
+      </StyledButton>
+      <StyledButton type="button" name="neutral" onClick={onLeaveFeedback}>
         Neutral
-      </button>
-      <button type="button" name="bad" onClick={onLeaveFeedback}>
+      </StyledButton>
+      <StyledButton type="button" name="bad" onClick={onLeaveFeedback}>
         Bad
-      </button>
+      </StyledButton>
     </>
   );
 };
---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  cursor: pointer;
+  border: none;
+  background-color: #5c5cc8;
+  border-radius: 10px;
+  padding: 5px 10px;
+  color: white;
+  margin-right: 10px;
+`;
